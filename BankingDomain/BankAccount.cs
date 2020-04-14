04ebf369150652063da3de785e70e58029698e10
Robert Galinas <rgalinas26@gmail.com>
2020-04-14 19:42:53 +0000

    Day 1

---
+++ BankingDomain/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankingDomain
+{   
+    public class BankAccount
+    {
+        private decimal balance = 1200;
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        public void Deposit(decimal amountToDeposit)
+        {
+            balance += amountToDeposit;
+        }
+
+        public void Withdraw(decimal amountToWithdraw)
+        {
+            balance -= amountToWithdraw;
+        }
+    }
+}
---
+++ BankingTests/BankAccountTests.cs
@@ -0,0 +1,45 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingTests
+{
+    public class BankAccountTests
+    {
+        [Fact]
+        public void NewAccountsHaveAppropriateBalance()
+        {
+            BankAccount account = new BankAccount();
+            decimal balance = account.GetBalance();
+
+            Assert.Equal(1200M, balance);
+        }
+
+        [Fact]
+        public void DepositingIncreasesBalance()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            var amountToDeposit = 100M;
+
+            account.Deposit(amountToDeposit);
+
+            Assert.Equal(openingBalance + 100M, account.GetBalance());
+        }
+
+        [Fact]
+        public void WithdrawlsDecreaseBalance()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            var amountToWithdraw = 100M;
+
+            account.Withdraw(amountToWithdraw);
+
+            Assert.Equal(openingBalance - 100M, account.GetBalance());
+
+        }
+    }
+}
