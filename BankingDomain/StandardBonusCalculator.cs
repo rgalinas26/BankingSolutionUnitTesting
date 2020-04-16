94eda15f0307c8a7500d22a2c9bfaf95d6fc28d9
Robert Galinas <rgalinas26@gmail.com>
2020-04-16 11:00:55 -0400

    time based bonus calculator

---
+++ BankingDomain/StandardBonusCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingDomain
+{
+    public class StandardBonusCalculator
+    {
+        public decimal CalculateBonusUsingStandardAlgorithm(decimal balance, decimal amount)
+        {
+            // If the balance is above a certain cutoff & the time is before the close of day
+            // they get a 10% bonus on deposits. If it is above the cutoff, and after they close
+            // of the day, they get 5%
+            if (hasBonusWorthyBalance(balance) && BeforeCutoff())
+            {
+                return amount * .10M;
+            }
+            if (balance > 10000)
+            {
+                return amount * .05M;
+            }
+            return 0;
+        }
+
+        private bool hasBonusWorthyBalance(decimal balance)
+        {
+            return balance > 10000;
+        }
+
+
+        protected virtual bool BeforeCutoff()
+        {
+            return DateTime.Now.Hour <= 16;
+        }
+    }
+}
---
+++ BankingTests/StandardBonusCalculatorTests.cs
@@ -0,0 +1,57 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingTests
+{
+    public class StandardBonusCalculatorTests
+    {
+        [Fact]
+        public void AboveCutoffAndBeforeClose()
+        {
+            var bonusCalculator = new TestingBonusCalculator(true);
+
+            var bonus = bonusCalculator.CalculateBonusUsingStandardAlgorithm(10001, 100);
+
+            Assert.Equal(10M, bonus);
+
+        }
+
+        [Fact]
+        public void AboveCutoffAfterClose()
+        {
+            var bonusCalculator = new TestingBonusCalculator(false);
+
+            var bonus = bonusCalculator.CalculateBonusUsingStandardAlgorithm(10001, 100);
+
+            Assert.Equal(5M, bonus);
+
+        }
+
+        [Fact]
+        public void BelowCutoffAfterClose()
+        {
+            var bonusCalculator = new TestingBonusCalculator(false);
+
+            var bonus = bonusCalculator.CalculateBonusUsingStandardAlgorithm(999, 100);
+
+            Assert.Equal(0, bonus);
+
+        }
+    }
+
+    public class TestingBonusCalculator : StandardBonusCalculator
+    {
+        private bool IsBeforeCutoff;
+        public TestingBonusCalculator(bool isBeforeCutoff)
+        {
+            IsBeforeCutoff = isBeforeCutoff;
+        }
+        protected override bool BeforeCutoff()
+        {
+            return IsBeforeCutoff;
+        }
+    }
+}
